759464dc897fc4f2d6deb3c931d565367d43b10f
oscar011 <oscar11011996@hotmail.com>
2020-02-07 10:36:49 -0600

    Create Calculadora.cs

---
+++ Tarea2/Tarea2/Calculadora.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calificaciones
+{
+    public static class Calculadora
+    {
+        public static int promedio(int proyecto, int tareas, int participacion)
+        {
+            if (participacion > 5)
+            {
+                return (proyecto * 60 + tareas * 40) + 80;
+            }
+            return (proyecto * 60 + tareas * 40);
+
+        }
+
+    }
+}
