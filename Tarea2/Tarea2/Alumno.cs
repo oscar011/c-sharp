f9a955457c448fc0db1e8a5a9056a4727428c8a9
oscar011 <oscar11011996@hotmail.com>
2020-02-07 10:36:26 -0600

    Create Alumno.cs

---
+++ Tarea2/Tarea2/Alumno.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calificaciones
+{
+    class Alumno
+    {
+        public string nombre, apPaterno, apMaterno; public int proyecto, tareas, participacion, promedio;
+
+        public string Nombre { get; set; }
+        public string ApPaterno { get; set; }
+        public string ApMaterno { get; set; }
+        public int Proyecto { get; set; }
+        public int Tareas { get; set; }
+        public int Participacion { get; set; }
+        public int Promedio { get; set; }
+
+
+        public Alumno()
+        {
+        }
+
+    }
+}
