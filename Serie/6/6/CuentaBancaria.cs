97bdecc177ca1ffa0fd30c5003ac7245f5e57382
oscar011 <oscar11011996@hotmail.com>
2020-02-09 20:13:54 -0600

    666666

---
+++ Serie/6/6/CuentaBancaria.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _6
+{
+    class CuentaBancaria
+    {
+        //declaramos variables de los usuarios
+        string nombre;
+        decimal saldo;
+
+        //constructor que recibe el nombre y el saldo
+        public CuentaBancaria(string nombre, decimal saldo)
+        {
+            this.nombre = nombre;
+            this.saldo = saldo;
+
+        }
+        //muestra el nombre y el saldo
+        public void mostrarInformacion()
+        {
+            Console.WriteLine("El usuario {0} tiene de saldo: {1}", nombre, saldo);
+        }
+        //suma lo que hay en saldo con un nuevo deposito
+        public void deposito(decimal deposito)
+        {
+            saldo += deposito;
+            mostrarInformacion();
+        }
+        //resta del saldo un retiro, pero verifica si tiene la cantidad que quiere retirar
+        public void retiro(decimal retiro)
+        {
+            if (saldo<retiro)
+            {
+                Console.WriteLine("No tienes esa cantidad");
+            }
+            else
+            {
+                saldo -= retiro;
+                mostrarInformacion();
+            }
+
+
+        }
+
+
+
+    }
+}
---
+++ Serie/6/6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creacion de dos objetos tipo cuentaBancaria
+            CuentaBancaria c1 = new CuentaBancaria("Oscar", 1000000);
+            CuentaBancaria c2 = new CuentaBancaria("Ignacio", 500000);
+
+            c1.mostrarInformacion();
+            c1.deposito(100);
+            c1.retiro(200);
+
+            c2.mostrarInformacion();
+            c2.deposito(200);
+            c2.retiro(100);
+
+            Console.ReadKey();
+        }
+    }
+}
