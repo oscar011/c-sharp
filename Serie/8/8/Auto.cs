bc53fe8e1b6f4d4c7f4869866c3da8da03f86c88
oscar011 <oscar11011996@hotmail.com>
2020-02-10 21:27:05 -0600

    ejercicio8

---
+++ Serie/8/8/Auto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _8
+{
+    class Auto
+    {
+        string modelo;
+        string color;
+        string velocidad;
+        public Auto() { }
+
+        public Auto(string modelo, string color, string velocidad)
+        {
+            Modelo = modelo;
+            Color = color;
+            Velocidad = velocidad;
+        }
+
+        public string Modelo { get; set; }
+        public string Color { get; set; }
+        public string Velocidad { get; set; }
+        
+        public void Acelerar()
+        {
+            Console.WriteLine("El auto tiene velocidad de {0}", velocidad);
+        }
+        public void Medir(int peso)
+        {
+            Console.WriteLine("El auto {0} pesa {1}", modelo, peso);
+        }
+        public void informacion()
+        {
+            Console.WriteLine("El auto {0} es de color {1}", modelo, color);
+        }
+    }
+}
---
+++ Serie/8/8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto auto1 = new Auto();
+            Auto auto2 = new Auto("Fiesta", "rojo", "100km");
+            Auto auto3 = new Auto("A3", "azul", "100km");
+
+            auto1.Modelo="A1";
+            auto1.Color = "negro";
+            auto1.Velocidad = "100km";
+            auto1.informacion();
+
+            auto2.Velocidad = "300.00";
+            auto2.Acelerar();
+
+            auto3.Medir(500);
+        }
+    }
+}
