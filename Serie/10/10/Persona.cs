f1984964d20126ceb8b2d8dc875eb390767c4b00
oscar011 <oscar11011996@hotmail.com>
2020-02-10 21:28:08 -0600

    ejercicio10

---
+++ Serie/10/10/Persona.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _10
+{
+    class Persona
+    {
+        string nombre;
+        int edad;
+        double estatura;
+        double peso;
+
+        public Persona(string nombre, int edad, double estatura, double peso)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.estatura = estatura;
+            this.peso = peso;
+        }
+
+        public double IMC()
+        {
+            return peso / (Math.Pow(estatura, 2));
+        }
+
+        public void Calcular(double IMC)
+        {
+            if (IMC >= 18.5 && IMC <= 24.9)
+            {
+                Console.WriteLine("Peso ideal");
+            }
+            else if (IMC < 18.5)
+            {
+                Console.WriteLine("Bajo peso");
+            }
+            else if (IMC > 24.9)
+            {
+                Console.WriteLine("Sobrepeso");
+            }
+        }
+        public void Informacion()
+        {
+            Console.WriteLine("{0} tiene {1} años, pesa {2} kilos y mide {3} mts.", nombre, edad, peso, estatura);
+        }
+    }
+}
---
+++ Serie/10/10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            int edad;
+            double estatura;
+            double peso;
+
+            Console.WriteLine("Escribe el nombre de la persona");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Escribe su edad");
+            edad = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escribe su estatuta en metros");
+            estatura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Escribe su peso en kilogramos");
+            peso = Convert.ToDouble(Console.ReadLine());
+            
+            Persona p = new Persona(nombre, edad, estatura, peso);
+            p.Informacion();
+            p.IMC();
+            Console.WriteLine("El IMC de la persona es: {0}", p.IMC());
+            p.Calcular(p.IMC());
+            Console.ReadKey();
+        }
+    }
+}
