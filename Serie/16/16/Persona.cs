c81421d951501955ab03b4f8389b778f123a527d
oscar011 <oscar11011996@hotmail.com>
2020-02-10 21:30:36 -0600

    ejercicio16

---
+++ Serie/16/16/Persona.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _16
+{
+    public abstract class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+
+        public abstract string Saludar();
+    }
+    public class Alumno : Persona
+    {
+        public Alumno(string nombre, int edad)
+        {
+            this.Nombre = nombre;
+            this.Edad = edad;
+        }
+        public override string Saludar()
+        {
+            string saludo = "Mi nombre es " + this.Nombre + ", soy un alumno y tengo "+ this.Edad + " años";
+            return saludo;
+        }
+    }
+    public class Empleado : Persona
+    {
+        public Empleado(string nombre, int edad)
+        {
+            this.Nombre = nombre;
+            this.Edad = edad;
+        }
+        public override string Saludar()
+        {
+            string saludo = "Mi nombre es " + this.Nombre + ", soy un empleado y tengo "+ this.Edad +" años";
+            return saludo;
+        }
+    }
+}
---
+++ Serie/16/16/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p1 = new Alumno("Oscar", 30);
+            Persona p2 = new Empleado("alvaro", 30);
+            Console.WriteLine(p1.Saludar());
+            Console.WriteLine(p2.Saludar());
+            Console.ReadKey();
+        }
+    }
+}
