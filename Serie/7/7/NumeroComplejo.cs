ffdb6370b7242505e4a21a1fa8304f5f6bbd55b0
oscar011 <oscar11011996@hotmail.com>
2020-02-10 21:26:04 -0600

    ejercicio7

---
+++ Serie/7/7/NumeroComplejo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _7
+{
+    class NumeroComplejo
+    {
+        int parteReal;
+        int parteImaginaria;
+
+        public NumeroComplejo(int parteReal, int parteImaginaria)
+        {
+            this.parteReal = parteReal;
+            this.parteImaginaria = parteImaginaria;
+        }
+
+        //suma de la parte real y la imaginaria respectivamente
+        public static NumeroComplejo operator +(NumeroComplejo nc1, NumeroComplejo nc2)
+        {
+            NumeroComplejo nc= new NumeroComplejo(nc1.parteReal + nc2.parteReal, nc1.parteImaginaria + nc2.parteImaginaria);
+            return nc;
+        }
+        //imprimir las dos partes
+        public void Imprimir()
+        {
+            Console.WriteLine("{0}, {1}i",parteReal , parteImaginaria);
+        }
+    }
+}
---
+++ Serie/7/7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumeroComplejo n1 = new NumeroComplejo(1, 8);
+            NumeroComplejo n2 = new NumeroComplejo(9, 5);
+            NumeroComplejo n3 = n1 + n2;
+            Console.WriteLine("Numeros");
+            n1.Imprimir();
+            n2.Imprimir();
+
+            Console.WriteLine("Imprimir suma");
+            n3.Imprimir();
+            Console.ReadKey();
+        }
+    }
+}
